0115af87f5b91e6086164f4f87aec8193ba80c11
Javierbm28 <javier_ericbravo@ceroca.cat>
2024-04-18 17:50:09 +0200

    Implementación Recuperación Contraseña

--- lib/pagines/Pagina_Recuperacio.dart
+++ lib/pagines/Pagina_Recuperacio.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:freetour/components/textField_auth.dart';
 import 'package:freetour/pagines/Pagina_Login.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:firebase_auth/firebase_auth.dart'; // Importar firebase_auth
 
 class RecuperarContrasenya extends StatefulWidget {
   const RecuperarContrasenya({Key? key});
@@ -12,9 +12,6 @@ class RecuperarContrasenya extends StatefulWidget {
 
 class _RecuperarContrasenyaState extends State<RecuperarContrasenya> {
   TextEditingController controladorEmail = TextEditingController();
-  TextEditingController controladorContrasenya = TextEditingController();
-
-  bool boton = true;
 
   @override
   Widget build(BuildContext context) {
@@ -31,14 +28,12 @@ class _RecuperarContrasenyaState extends State<RecuperarContrasenya> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              Text(
+              const Text(
                 "Recuperar Contraseña",
-                style: GoogleFonts.aBeeZee(
-                  textStyle: const TextStyle(
-                    fontSize: 70,
-                    fontWeight: FontWeight.bold,
-                    color: Color.fromARGB(255, 63, 214, 63),
-                  ),
+                style: TextStyle(
+                  fontSize: 70,
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(255, 63, 214, 63),
                 ),
               ),
               const SizedBox(height: 50,),
@@ -47,36 +42,52 @@ class _RecuperarContrasenyaState extends State<RecuperarContrasenya> {
                 obscureText: false,
                 labelText: "Email",
               ),
-              const SizedBox(height: 50,),
-              TextFieldAuth(
-                controller: controladorContrasenya,
-                obscureText: true,
-                labelText: "Nueva Contraseña",
-              ),
-              const SizedBox(height: 100,),
-              const Icon(
-                Icons.recommend,
-                size: 80,
-              ),
-              const Text(
-                "Recuerda apuntarte la nueva contraseña ;)",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Colors.green,
-                ),
-              ),
               const SizedBox(height: 100,),
               ElevatedButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => Login(
-                        alFerClic: () {}, 
-                      ),
-                    ),
-                  );
+                onPressed: () async {
+                  try {
+                    // Enviar correo para restablecer la contraseña
+                    await FirebaseAuth.instance.sendPasswordResetEmail(
+                      email: controladorEmail.text,
+                    );
+                    // Mostrar mensaje de éxito
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: const Text("Éxito"),
+                          content: const Text("Se ha enviado un correo para restablecer la contraseña."),
+                          actions: <Widget>[
+                            ElevatedButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                              child: const Text('Aceptar'),
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  } catch (e) {
+                    // Mostrar mensaje de error
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: const Text("Error"),
+                          content: Text("Error al enviar el correo: $e"),
+                          actions: <Widget>[
+                            ElevatedButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                              child: const Text('Aceptar'),
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  }
                 },
                 child: const Text("Recuperar contraseña"),
               ),
@@ -87,3 +98,4 @@ class _RecuperarContrasenyaState extends State<RecuperarContrasenya> {
     );
   }
 }
+
