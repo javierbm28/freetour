111bd267edbff24ae8c91bd64028af64929beab7
Javierbm28 <javier_ericbravo@ceroca.cat>
2024-05-09 15:52:18 +0200

    Agregar Ubicaciónes
    Se ha implementado la función de agregar puntos de interés y se guarde en Firestore. (Los filtros aun no funcionan)

--- lib/pagines/FilterableMap.dart
+++ lib/pagines/FilterableMap.dart
@@ -5,8 +5,8 @@ import 'package:geolocator/geolocator.dart';
 import 'dart:typed_data';
 import 'package:flutter/services.dart' show rootBundle;
 import 'dart:async';
-import 'dart:ui'; 
-import 'dart:math'; 
+import 'dart:ui';
+import 'dart:math';
 
 enum PlaceType {
   restaurant,
@@ -15,6 +15,13 @@ enum PlaceType {
   bar,
 }
 
+Map<PlaceType, bool> filters = {
+  PlaceType.restaurant: false,
+  PlaceType.monument: false,
+  PlaceType.nightclub: false,
+  PlaceType.bar: false,
+};
+
 class FilterableMap extends StatefulWidget {
   @override
   _FilterableMapState createState() => _FilterableMapState();
@@ -22,22 +29,25 @@ class FilterableMap extends StatefulWidget {
 
 class _FilterableMapState extends State<FilterableMap> {
   MapboxMapController? mapController;
-  final LatLng defaultCenter = const LatLng(41.3851, 2.1734); // Barcelona coordinates
+  final LatLng defaultCenter =
+      const LatLng(41.3851, 2.1734); // Barcelona coordinates
+  Symbol? lastAddedSymbol;
 
-  Map<PlaceType, bool> filters = {
-    PlaceType.restaurant: false,
-    PlaceType.monument: false,
-    PlaceType.nightclub: false,
-    PlaceType.bar: false,
-  };
-
-  DateTime? lastTap;
+  TextEditingController nameController = TextEditingController();
+  String? selectedType;
 
   @override
   void initState() {
     super.initState();
     _determinePosition();
     _loadPointerImage(); // Load the pointer image on initialization
+  }
+
+  void _toggleFilter(PlaceType type) {
+    setState(() {
+      filters[type] = !filters[type]!;
+      _updateMap(); // Si necesitas actualizar el mapa después de cambiar el filtro
+    });
   }
 
   void _onMapCreated(MapboxMapController controller) async {
@@ -51,30 +61,114 @@ class _FilterableMapState extends State<FilterableMap> {
     mapController!.addImage('icon-user', list);
   }
 
-  // Function to load the pointer image
   Future<void> _loadPointerImage() async {
     final ByteData bytes = await rootBundle.load('lib/images/Puntero.png');
     final Uint8List list = bytes.buffer.asUint8List();
     mapController!.addImage('puntero', list);
   }
 
-  void _onMapClicked(Point<double> point, LatLng latLng) {
+  DateTime? lastTap;
+  void _onMapClicked(Point<double> point, LatLng latLng) async {
     final DateTime now = DateTime.now();
-    if (lastTap != null && now.difference(lastTap!) < Duration(milliseconds: 500)) {
-      // Place the puntero image on double tap
-      mapController?.addSymbol(SymbolOptions(
+    if (lastTap != null &&
+        now.difference(lastTap!) < Duration(milliseconds: 500)) {
+      lastAddedSymbol = await mapController?.addSymbol(SymbolOptions(
         geometry: latLng,
         iconImage: 'puntero',
         iconSize: 0.08, // Adjust size as needed
       ));
+      _showAddNewPlaceDialog(latLng);
       lastTap = null; // Reset last tap
     } else {
       lastTap = now;
     }
   }
 
+  void _updateMap() {
+    if (mapController == null) return;
+
+    mapController!.clearSymbols(); // Limpia todos los símbolos existentes
+    FirebaseFirestore.instance
+        .collection('locations')
+        .where('type',
+            isEqualTo: filters.entries
+                .where((entry) => entry.value)
+                .map((entry) => entry.key.toString().split('.').last)
+                .join(','))
+        .get()
+        .then((querySnapshot) {
+      for (var doc in querySnapshot.docs) {
+        GeoPoint geoPoint = doc['coordinates'];
+        mapController!.addSymbol(SymbolOptions(
+            geometry: LatLng(geoPoint.latitude, geoPoint.longitude),
+            iconImage: "${doc['type']}-icon"));
+      }
+    });
+  }
+
   Future<void> _showAddNewPlaceDialog(LatLng latLng) async {
-    // Example implementation here
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // User must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Nueva Ubicación'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                TextField(
+                  controller: nameController,
+                  decoration: InputDecoration(hintText: "Nombre del lugar"),
+                ),
+                DropdownButton<String>(
+                  value: selectedType,
+                  hint: Text("Selecciona un tipo"),
+                  isExpanded: true,
+                  items: PlaceType.values.map((type) {
+                    return DropdownMenuItem<String>(
+                      value: type.toString().split('.').last,
+                      child: Text(type.toString().split('.').last),
+                    );
+                  }).toList(),
+                  onChanged: (value) {
+                    setState(() {
+                      selectedType = value;
+                    });
+                  },
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Cancelar'),
+              onPressed: () {
+                if (lastAddedSymbol != null) {
+                  mapController?.removeSymbol(lastAddedSymbol!);
+                }
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: Text('Guardar Ubicación'),
+              onPressed: () {
+                if (nameController.text.isNotEmpty && selectedType != null) {
+                  FirebaseFirestore.instance.collection('locations').add({
+                    'name': nameController.text,
+                    'type': selectedType,
+                    'coordinates': GeoPoint(latLng.latitude, latLng.longitude)
+                  });
+                  Navigator.of(context).pop();
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                      content: Text("Todos los campos son obligatorios")));
+                }
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 
   Future<void> _determinePosition() async {
@@ -83,7 +177,8 @@ class _FilterableMapState extends State<FilterableMap> {
 
     serviceEnabled = await Geolocator.isLocationServiceEnabled();
     if (!serviceEnabled) {
-      await _addUserLocationSymbol(defaultCenter); // Use default center if services are not enabled
+      await _addUserLocationSymbol(
+          defaultCenter); // Use defaultCenter if services are not enabled
       return;
     }
 
@@ -95,17 +190,15 @@ class _FilterableMapState extends State<FilterableMap> {
         return;
       }
     }
-    
+
     if (permission == LocationPermission.deniedForever) {
       await _addUserLocationSymbol(defaultCenter);
       return;
     }
 
-    // Obtaining current location with high accuracy and a timeout
     Position position = await Geolocator.getCurrentPosition(
-      desiredAccuracy: LocationAccuracy.best,
-      timeLimit: Duration(seconds: 10)
-    );
+        desiredAccuracy: LocationAccuracy.best,
+        timeLimit: Duration(seconds: 10));
     await _addUserLocationSymbol(LatLng(position.latitude, position.longitude));
   }
 
@@ -119,33 +212,6 @@ class _FilterableMapState extends State<FilterableMap> {
     }
   }
 
-  void _toggleFilter(PlaceType type) {
-    setState(() {
-      filters[type] = !filters[type]!;
-      _updateMap();
-    });
-  }
-
-  void _updateMap() {
-    mapController?.clearSymbols();
-    FirebaseFirestore.instance
-      .collection('locations')
-      .where('type',
-          isEqualTo: filters.entries
-              .where((entry) => entry.value)
-              .map((entry) => entry.key.toString().split('.').last)
-              .join(','))
-      .get()
-      .then((querySnapshot) {
-        for (var doc in querySnapshot.docs) {
-          GeoPoint geoPoint = doc['coordinates'];
-          mapController?.addSymbol(SymbolOptions(
-              geometry: LatLng(geoPoint.latitude, geoPoint.longitude),
-              iconImage: "${doc['type']}-icon"));
-        }
-      });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -153,22 +219,28 @@ class _FilterableMapState extends State<FilterableMap> {
         children: <Widget>[
           Expanded(
             child: MapboxMap(
-              accessToken: "pk.eyJ1IjoiamF2aWVyY2Vyb2NhIiwiYSI6ImNsdnBhNG92YzBqd2Iya2sxeXYxeWUyYWkifQ.DSim5b1yxSAJjQioCrMDpQ",
+              accessToken:
+                  "pk.eyJ1IjoiamF2aWVyY2Vyb2NhIiwiYSI6ImNsdnBhNG92YzBqd2Iya2sxeXYxeWUyYWkifQ.DSim5b1yxSAJjQioCrMDpQ",
               onMapCreated: _onMapCreated,
-              initialCameraPosition: CameraPosition(target: defaultCenter, zoom: 14),
+              initialCameraPosition:
+                  CameraPosition(target: defaultCenter, zoom: 14),
               onMapClick: _onMapClicked,
             ),
           ),
           Row(
-            children: PlaceType.values.map((type) => Expanded(
-              child: TextButton(
-                onPressed: () => _toggleFilter(type),
-                child: Text(type.toString().split('.').last),
-                style: TextButton.styleFrom(
-                  backgroundColor: filters[type] ?? false ? Colors.blue : Colors.grey,
-                ),
-              ),
-            )).toList(),
+            children: PlaceType.values
+                .map((type) => Expanded(
+                      child: TextButton(
+                        style: TextButton.styleFrom(
+                          backgroundColor: filters[type] ?? false
+                              ? Colors.blue
+                              : Colors.grey,
+                        ),
+                        onPressed: () => _toggleFilter(type),
+                        child: Text(type.toString().split('.').last),
+                      ),
+                    ))
+                .toList(),
           )
         ],
       ),
