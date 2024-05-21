045db72bbf63c8714644d9c67f6dde848e4ac29d
Javierbm28 <javier_ericbravo@ceroca.cat>
2024-05-21 14:00:43 +0200

    Retoque pagina inicio
    Se agregado un video de fondo

--- lib/pagines/Pagina_Inici.dart
+++ lib/pagines/Pagina_Inici.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:freetour/components/boto_auth.dart';
 import 'package:freetour/pagines/MostrarDatos.dart';
 import 'package:freetour/pagines/Pagina_Login.dart';
 import 'package:freetour/pagines/FilterableMap.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:freetour/pagines/Pagina_editar_dades.dart';
+import 'package:video_player/video_player.dart';
 
 class PaginaInici extends StatefulWidget {
-  const PaginaInici({
-    Key? key,
-  }) : super(key: key);
+  const PaginaInici({Key? key}) : super(key: key);
 
   @override
   State<PaginaInici> createState() => _PaginaIniciState();
@@ -18,15 +17,31 @@ class PaginaInici extends StatefulWidget {
 
 class _PaginaIniciState extends State<PaginaInici> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.asset("lib/images/video_fondo.mp4")
+      ..initialize().then((_) {
+        setState(() {});
+        _controller.play();
+        _controller.setLooping(true);
+      });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   void logout() async {
     await _auth.signOut();
     Navigator.pushReplacement(
       context,
-      MaterialPageRoute(
-        builder: (context) => const Login(),
-      ),
-    ); // Regresa a la pantalla de login después de cerrar sesión
+      MaterialPageRoute(builder: (context) => const Login()),
+    );
   }
 
   @override
@@ -34,7 +49,7 @@ class _PaginaIniciState extends State<PaginaInici> {
     final user = _auth.currentUser;
 
     return WillPopScope(
-      onWillPop: () async => false, // Evitar retroceso
+      onWillPop: () async => false,
       child: Scaffold(
         appBar: AppBar(
           title: const Text("Discovery"),
@@ -44,9 +59,7 @@ class _PaginaIniciState extends State<PaginaInici> {
               onPressed: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(
-                    builder: (context) => Datos(),
-                  ),
+                  MaterialPageRoute(builder: (context) => Datos()),
                 );
               },
               icon: const Icon(Icons.person),
@@ -59,76 +72,79 @@ class _PaginaIniciState extends State<PaginaInici> {
         ),
         body: user == null
             ? Center(child: Text('No hay usuario autenticado'))
-            : SingleChildScrollView(
-                child: Padding(
-                  padding: const EdgeInsets.all(0.0),
-                  child: Container(
-                    decoration: const BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage("assets/foto_fondo.jpg"),
+            : Stack(
+                children: [
+                  if (_controller.value.isInitialized)
+                    Positioned.fill(
+                      child: FittedBox(
                         fit: BoxFit.cover,
+                        child: SizedBox(
+                          width: _controller.value.size.width,
+                          height: _controller.value.size.height,
+                          child: VideoPlayer(_controller),
+                        ),
                       ),
                     ),
-                    child: Column(
-                      children: [
-                        FutureBuilder<DocumentSnapshot>(
-                          future: FirebaseFirestore.instance
-                              .collection('users')
-                              .doc(user.uid)
-                              .get(),
-                          builder: (context, snapshot) {
-                            if (snapshot.connectionState == ConnectionState.waiting) {
-                              return CircularProgressIndicator();
-                            }
-                            if (snapshot.hasError) {
-                              return Text('Error al obtener los datos');
-                            }
-                            final data = snapshot.data!.data() as Map<String, dynamic>;
-                            final String nombre = data['nombre'] ?? '';
-                            final String apellidos = data['apellidos'] ?? '';
-                            return Center(
-                              child: Text(
-                                "$nombre $apellidos",
-                                style: const TextStyle(
-                                  fontSize: 50,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white,
+                  SingleChildScrollView(
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          FutureBuilder<DocumentSnapshot>(
+                            future: FirebaseFirestore.instance
+                                .collection('users')
+                                .doc(user.uid)
+                                .get(),
+                            builder: (context, snapshot) {
+                              if (snapshot.connectionState == ConnectionState.waiting) {
+                                return CircularProgressIndicator();
+                              }
+                              if (snapshot.hasError) {
+                                return Text('Error al obtener los datos');
+                              }
+                              final data = snapshot.data!.data() as Map<String, dynamic>;
+                              final String nombre = data['nombre'] ?? '';
+                              final String apellidos = data['apellidos'] ?? '';
+                              return Center(
+                                child: Text(
+                                  "$nombre $apellidos",
+                                  style: const TextStyle(
+                                    fontSize: 50,
+                                    fontWeight: FontWeight.bold,
+                                    color: Color.fromARGB(255, 45, 255, 3),
+                                  ),
                                 ),
-                              ),
-                            );
-                          },
-                        ),
-                        const SizedBox(
-                          height: 30,
-                        ),
-                        const Text(
-                          "Bienvenido/a",
-                          style: TextStyle(
-                            fontSize: 50,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
+                              );
+                            },
                           ),
-                        ),
-                        const SizedBox(
-                          height: 245,
-                        ),
-                        BotoAuth(
-                          text: "Ir a mapa",
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => FilterableMap(),
-                              ),
-                            );
-                          },
-                        ),
-                      ],
+                          const SizedBox(height: 30),
+                          const Text(
+                            "Bienvenido/a",
+                            style: TextStyle(
+                              fontSize: 50,
+                              fontWeight: FontWeight.bold,
+                              color: Color.fromARGB(255, 45, 255, 3),
+                            ),
+                          ),
+                          const SizedBox(height: 245),
+                          BotoAuth(
+                            text: "Ir a mapa",
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) => FilterableMap()),
+                              );
+                            },
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                ),
+                ],
               ),
       ),
     );
   }
 }
+
